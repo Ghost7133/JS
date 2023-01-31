9277f281ecebbf1d4f6182758697ac189e8be3a0
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-01-31 08:25:58 +0300

    Update zad1.js

--- ДЗ_Числа/zad1.js
+++ ДЗ_Числа/zad1.js
@@ -2,7 +2,7 @@
 "Use strict";
 const number = 999;
 const firstNumber = Math.floor(number / 100);
-const secondNumber = ((number % 100) - ((number % 100) % 10)) / 10;
-const thirdNumber = (number % 100) % 10;
+const secondNumber = ((number % 100) - (number % 10)) / 10;
+const thirdNumber = number % 10;
 console.log("Произведение цифр заданного трехзначного числа",number,"это -",firstNumber * secondNumber * thirdNumber);
 console.log("Сумма цифр заданного трехзначного числа",number,"это -",firstNumber + secondNumber + thirdNumber);
