62b81a753f2d58b4a721e7932818455ee254771c
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-01-31 08:24:22 +0300

    Update zad2.js

--- ДЗ_Числа/zad2.js
+++ ДЗ_Числа/zad2.js
@@ -2,5 +2,5 @@
 "use strict";
 const number = 67;
 const secondNumber = number % 10;
-const firstNumber = number - secondNumber;
-console.log("Первернутое число",number, "это число -",secondNumber * 10 + firstNumber / 10);
+const firstNumber = (number - secondNumber)/10;
+console.log("Первернутое число",number, "это число -",secondNumber * 10 + firstNumber);
