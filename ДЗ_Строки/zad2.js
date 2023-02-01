ebd730f9a8dbe5accd65f0edb726ee914adead5f
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-01 14:27:15 +0300

    Update zad2.js

--- ДЗ_Строки/zad2.js
+++ ДЗ_Строки/zad2.js
@@ -1,6 +1,6 @@
 // Write a JavaScript function to convert a string into lower camel case
 // JavaScript Exercises -> javaScriptExercises
 "use strict";
-const string = "JavaScript Exercises"
+const string = "JavaScript еxercises"
 
-console.log(string[string.indexOf("J")].toLowerCase() + string.slice(1,string.indexOf(" ")) + string.slice(string.indexOf(" ") + 1))
+console.log(string.slice(0,string.indexOf(" ")) + (string.slice(string.indexOf(" ")+1,string.indexOf(" ")+2).toUpperCase()) + string.slice(string.indexOf(" ") + 2))
