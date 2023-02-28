db9e5210e852232d35259a191c9c29f6da819a65
Lushious <122861023+Lushiousi@users.noreply.github.com>
2023-02-28 08:54:17 +0300

    Add files via upload

---
+++ js-samrab1/3.js
@@ -0,0 +1,8 @@
+let number = 3;
+if(number = 0) {
+console.log("Вы ввели число 0");
+} else if (number = 1) {
+console.log("Вы ввели число 1");
+} else {
+    console.log("Вы ввели число 2, а может и 3");
+} 
---
+++ js-samrab1/5.js
@@ -0,0 +1,20 @@
+let numberday = 8
+
+if (numberday == 1) {
+console.log("Понедельник");    
+} else if (numberday == 2) {
+    console.log("Вторник");
+} else if (numberday == 3) {
+    console.log("Среда");
+} else if (numberday == 4) {
+    console.log("Четверг");
+}  else if (numberday == 5) {
+    console.log("Пятница");
+} else if (numberday == 6) {
+    console.log("Суббота");
+} else if (numberday == 7) {
+    console.log("Воскресенье");
+} else {
+    console.log("numberday - не является числом для дней недели");
+}
+
