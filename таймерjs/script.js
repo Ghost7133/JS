f4721ed82a96745371b195a4a84237b287345c73
Lushious <122861023+Lushiousi@users.noreply.github.com>
2023-03-22 23:44:52 +0300

    Add files via upload

---
+++ таймерjs/script.js
@@ -0,0 +1,22 @@
+    let time = document.querySelector('.time');
+    let btnStart = document.querySelector('.start');
+    let score = document.querySelector('.timer');
+    let timer;
+
+    function startTimer() {
+      score.textContent -= 1;
+      if (score.textContent <= 0) {
+        timer = clearInterval(timer);
+      }
+    }
+
+    function clickOn() {
+      timer = clearInterval(timer);
+
+      if (time.value > 0) {
+        score.textContent = time.value;
+        timer = setInterval(startTimer, 1000)
+      } else {timer = clearInterval(timer)}
+    }
+
+    btnStart.addEventListener('click', clickOn);
