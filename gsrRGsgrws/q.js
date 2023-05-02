dd2dce8c3bf42e7d547c4f71748f12fd540037bc
Lushious <122861023+Lushiousi@users.noreply.github.com>
2023-05-02 11:16:07 +0300

    Add files via upload

---
+++ gsrRGsgrws/q.js
@@ -0,0 +1,93 @@
+// Задание 1
+
+// let arr = [1, 2, 3, 4, 5];
+// let test = arr.splice(3, 0, "a", "b", "c");
+// console.log(arr);
+
+// Задание 2
+
+// let obj = {
+//   js: "test",
+//   jq: "hello",
+//   css: "world",
+// };
+// console.log(Object.keys(obj));
+
+// Задание 3
+
+// let string = "http://phphtml";
+// if (string.slice(0, 7) == "http://") {
+//   console.log("Да");
+// } else {
+//   console.log("Нет");
+// }
+
+// Задание 4
+
+// let str = "index.html";
+// if (str.slice(-5) == ".html") {
+//   console.log("Да");
+// } else {
+//   console.log("Нет");
+// }
+
+// Задание 5
+
+// let str = "12345";
+// console.log(str.split("").reverse().join(""));
+
+// Задание 6
+
+// let str = "var_test_text";
+// console.log(
+//   "".concat(
+//     str.slice(0, str.indexOf("_")),
+//     str[str.indexOf("_") + 1].toUpperCase(),
+//     str.slice(str.indexOf("_") + 2, str.lastIndexOf("_")),
+//     str[str.indexOf("_") + 1].toUpperCase(),
+//     str.slice(str.lastIndexOf("_") + 2)
+//   )
+// );
+
+// Задание 7
+
+// let arr = ["я", "учу", "javascript", "!"];
+
+// let test = arr.join(" ");
+// console.log(test.toUpperCase());
+
+// Задание 8
+
+// let str = "2025-12-31";
+// let newDate = str.split("-").reverse().join("/");
+// console.log(newDate);
+
+// Задание 9
+
+// const fullName = "Иван Иванович Иванов";
+// const surname = "Иванов";
+// if (fullName.indexOf(surname, fullName.length - surname.length) >= 13) {
+//   console.log(surname, fullName.slice(0, fullName.length - surname.length));
+// } else {
+//   console.log("error");
+// }
+
+// Задание 10
+
+// let firstArr = [1, 2, 3, 4, 5];
+// let secondArr = [1, 2, 3, 4, 5];
+
+// function condition() {
+//   if (firstArr.length != secondArr.length) return false;
+//   for (let i = 0; i < firstArr.length; i++) {
+//     if (firstArr[i] !== secondArr[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// console.log(condition());
+
+// Задание 11
+
+
