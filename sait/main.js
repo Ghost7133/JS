aa32e3b3292b0d7152080ec13cfe058ebc78e8dc
Lushious <122861023+Lushiousi@users.noreply.github.com>
2023-09-11 07:18:40 +0300

    Add files via upload

---
+++ sait/main.js
@@ -0,0 +1,8 @@
+const iconLogin = document.querySelector(".icon")
+const modalAuth = document.querySelector(".modal-auth")
+
+
+iconLogin.addEventListener("click", () =>{
+    modalAuth.style.display = "flex"
+    console.log(123);
+})
