ed2074f2b7531ade789f2e30d95c8cb440d71224
Lushious <122861023+Lushiousi@users.noreply.github.com>
2023-09-11 14:04:37 +0300

    Delete sait directory

--- sait/main.js
+++
@@ -1,8 +0,0 @@
-const iconLogin = document.querySelector(".icon")
-const modalAuth = document.querySelector(".modal-auth")
-
-
-iconLogin.addEventListener("click", () =>{
-    modalAuth.style.display = "flex"
-    console.log(123);
-})
